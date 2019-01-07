630e3db5887ba0aa4b7e481599de866ef56ad3a0
Hoishin <hoishinxii@gmail.com>
2019-01-08 01:51:03 +0900

    initial commit

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+	arrowParens: 'always',
+	bracketSpacing: false,
+	endOfLine: 'lf',
+	useTabs: true,
+	trailingComma: 'all',
+	tabWidth: 4,
+	singleQuote: true,
+	semi: true,
+	printWidth: 80,
+	htmlWhitespaceSensitivity: 'css',
+	jsxBracketSameLine: false,
+	jsxSingleQuote: true,
+};
--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # prettierrc
+
 Hoishin's Prettier configuration
+
+## Install
+
+```
+$ npm i -D @hoishin/prettierrc
+```
+
+## Usage
+
+```js
+// .prettierrc.js
+module.exports = require('@hoishin/prettierrc')
+```
