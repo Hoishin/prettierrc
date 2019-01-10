53a612a296a93709a6438951d7c36a1423daf88f
Hoishin <hoishinxii@gmail.com>
2019-01-10 13:17:45 +0900

    refactor: use typescript

---
+++ src/prettierrc.ts
@@ -0,0 +1,18 @@
+import { Options } from "prettier";
+
+const options: Options = {
+	arrowParens: "always",
+	bracketSpacing: false,
+	endOfLine: "lf",
+	useTabs: true,
+	trailingComma: "all",
+	tabWidth: 4,
+	singleQuote: true,
+	semi: true,
+	printWidth: 80,
+	htmlWhitespaceSensitivity: "css",
+	jsxBracketSameLine: false,
+	jsxSingleQuote: true
+};
+
+export default options
